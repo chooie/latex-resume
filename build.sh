06b7399d3e94c51afabe472d6a5e53603fb133ac
chooie <charlie.hebert92@gmail.com>
2018-10-23 15:08:41 +0100

    Preserve build output in order to support labels (two-pass compile)

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-rm -rf build/*
-latexmk -cd --pdf -outdir=build/ charles_hebert_resume.tex
-find build/ ! -name 'charles_hebert_resume.pdf' -type f -exec rm -f {} +
+latexmk -pvc --pdf -outdir=build/ charles_hebert_resume.tex
